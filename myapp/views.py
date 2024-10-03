781a213ee4c7902021c13b7aa13bf34ede23fce1
Sawyoelayela <sawjoel2972001@gmail.com>
2024-10-03 11:22:33 +0630

    static

--- myapp/views.py
+++ myapp/views.py
@@ -3,6 +3,6 @@ from .models import *
 # Create your views here.
 
 def home(request):
-    home = Home.objects.all().order_by(-id)
+    home = Home.objects.all().order_by('-id')
 
     return render(request,'index.html', {'home':home})
